f2204dbafdbf586c434ffa4591522e5ba62dcfe8
Sury <134803136+Apatoma@users.noreply.github.com>
2025-02-28 13:47:53 +0100

    Update textWorker.js

--- js/textWorker.js
+++ js/textWorker.js
@@ -2,15 +2,24 @@ importScripts('./textProcessor.js');
 
 const humanizer = new TextHumanizer();
 
-self.addEventListener('message', (e) => {
+self.onmessage = (e) => {
     try {
+        const progressCallback = (progress) => {
+            self.postMessage({ progress });
+        };
+
         const result = humanizer.process(
             e.data.text,
             e.data.lang,
-            e.data.options
+            e.data.options,
+            progressCallback
         );
-        self.postMessage(result);
+
+        self.postMessage({ result });
     } catch (error) {
-        self.postMessage({ error: error.message });
+        self.postMessage({ 
+            error: error.message || 'Error desconocido',
+            stack: error.stack 
+        });
     }
-});
+};
